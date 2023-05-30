6f491ca15f050a654c488b99d4c9213b2b7894c0
Samuel Paulo <samuel_paulo125@hotmail.com>
2023-05-30 15:03:23 -0400

    teste 1

---
+++ teste1/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+Console.WriteLine("informe o primeiro valr");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("informe o segundo valor");
+int b = int.Parse(Console.ReadLine());
+int result = a / b;
+Console.WriteLine(result);
