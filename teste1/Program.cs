b564e4c970d9478fce61715ac42c24d55fb65b3f
Samuel Paulo <samuel_paulo125@hotmail.com>
2023-05-30 15:28:39 -0400

    test 1

--- teste1/Program.cs
+++ teste1/Program.cs
@@ -1,8 +1,14 @@
-﻿
+﻿try
+{
 
-Console.WriteLine("informe o primeiro valr");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("informe o segundo valor");
-int b = int.Parse(Console.ReadLine());
-int result = a / b;
-Console.WriteLine(result);
+
+    Console.WriteLine("informe o primeiro valr");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("informe o segundo valor");
+    int b = int.Parse(Console.ReadLine());
+    int result = a / b;
+    Console.WriteLine(result);
+}catch(Exception ex)
+{
+    Console.WriteLine("erro");
+}
